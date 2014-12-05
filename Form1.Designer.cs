c03d0a846e6ac6183dd4ccaf4b8813601a3a47d6
emrekose26 <emre.kose2@ogr.sakarya.edu.tr>
2014-12-05 23:58:35 +0200

    IMEI-No-Dogrulama
    IMEI-No-Dogrulama

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace IMEI_No_Dogrulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Kontrol = new System.Windows.Forms.Button();
+            this.lblBasamak = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IMEI NO :";
+            // 
+            // btn_Kontrol
+            // 
+            this.btn_Kontrol.Location = new System.Drawing.Point(17, 97);
+            this.btn_Kontrol.Name = "btn_Kontrol";
+            this.btn_Kontrol.Size = new System.Drawing.Size(194, 42);
+            this.btn_Kontrol.TabIndex = 2;
+            this.btn_Kontrol.Text = "Kontrol Et";
+            this.btn_Kontrol.UseVisualStyleBackColor = true;
+            this.btn_Kontrol.Click += new System.EventHandler(this.btn_Kontrol_Click);
+            // 
+            // lblBasamak
+            // 
+            this.lblBasamak.AutoSize = true;
+            this.lblBasamak.Location = new System.Drawing.Point(121, 56);
+            this.lblBasamak.Name = "lblBasamak";
+            this.lblBasamak.Size = new System.Drawing.Size(19, 13);
+            this.lblBasamak.TabIndex = 3;
+            this.lblBasamak.Text = "15";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kalan Karakter :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(206, 60);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Telefonununuzun IMEI kodunu \r\nnumara arama kısmına \r\n*#06# veya *#0000# \r\ngirerek" +
+    " öğrenebilirsiniz. ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btn_Kontrol);
+            this.groupBox1.Location = new System.Drawing.Point(35, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 145);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_Kontrol;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 232);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBasamak);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IMEI NO DOĞRULAMA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Kontrol;
+        private System.Windows.Forms.Label lblBasamak;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMEI_No_Dogrulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Kontrol_Click(object sender, EventArgs e)
+        {
+            long sayi;
+            int[] dizi = new int[15];
+            sayi = long.Parse(textBox1.Text);
+
+            if (textBox1.TextLength < 15)
+            {
+                MessageBox.Show("IMEI Numarası 0 ile 15 arasında olmalıdır");
+            }
+            else if(textBox1.TextLength == 15)
+            {
+                for (int i = 0; i < 15 ; i++)
+                {
+                    dizi[i] = Convert.ToInt32(sayi % 10);
+                    sayi = sayi / 10;
+                }
+
+                int onluk, birlik;
+                for (int i = 13; i >= 1; i-=2)
+                {
+                    dizi[i] = dizi[i] * 2;
+                    onluk = dizi[i] / 10;
+                    birlik = dizi[i] % 10;
+                    dizi[i] = onluk + birlik;
+                }
+
+
+                int toplam = 0;
+                for (int i = 1; i < 15; i++)
+                {
+                    toplam += dizi[i];
+                }
+
+                int sonuc = toplam % 10;
+
+                if (10 - sonuc == dizi[0])
+                    MessageBox.Show("IMEI NO GECERLİ");
+                else
+                    MessageBox.Show("GECERSİZ IMEI");
+            }
+       }
+           
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //rakam sınırlaması
+            if (textBox1.TextLength == 15)
+                e.Handled = true;
+            
+            //Harf girişi kısıtlaması
+            if (char.IsLetter(e.KeyChar))
+                e.Handled = true;
+            else
+                e.Handled = false;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lblBasamak.Text = Convert.ToString(15 - textBox1.TextLength);
+
+            if (textBox1.TextLength > 15)
+            {
+                MessageBox.Show("15'ten fazla giremezsiniz.Lutfen Tekrar Deneyin ");
+                textBox1.Text = "";
+            }
+            
+        }
+
+    }
+}
